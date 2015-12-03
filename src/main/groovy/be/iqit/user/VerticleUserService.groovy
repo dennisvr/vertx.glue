88e9c005d580d29256e2275139ad7fe9aa892e2c
Dennis Van Roeyen <dennis@iqit.be>
2015-12-03 23:08:55 +0100

    VerticleUserService using EventBuilder.consume

--- src/main/groovy/be/iqit/user/VerticleUserService.groovy
+++ src/main/groovy/be/iqit/user/VerticleUserService.groovy
@@ -1,8 +1,10 @@
 package be.iqit.user
 
+import be.iqit.convert.Converter
+import be.iqit.convert.ObjectMapperConverter
+import be.iqit.event.EventBuilder
 import io.vertx.core.AbstractVerticle
 import rx.Observable
-
 
 /**
  * Created by dvanroeyen on 30/11/15.
@@ -10,9 +12,20 @@ import rx.Observable
 class VerticleUserService extends AbstractVerticle implements UserService {
 
     UserRepository userRepository
+    Converter converter = new ObjectMapperConverter()
+    EventBuilder eventBuilder
 
     public VerticleUserService(UserRepository userRepository) {
         this.userRepository = userRepository
+    }
+
+    @Override
+    void start() throws Exception {
+        super.start()
+        eventBuilder = new EventBuilder(UserService, converter, vertx)
+        eventBuilder.consume(this.&getUser)
+        eventBuilder.consume(this.&saveUser)
+        eventBuilder.consume(this.&getUsers)
     }
 
     @Override
