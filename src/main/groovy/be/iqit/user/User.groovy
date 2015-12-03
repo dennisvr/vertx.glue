528fbca77ee73481dff7ea60df1ef3f7cb4fc7a6
Dennis Van Roeyen <dennis@iqit.be>
2015-12-03 23:03:42 +0100

    User jackson annotations

--- src/main/groovy/be/iqit/user/User.groovy
+++ src/main/groovy/be/iqit/user/User.groovy
@@ -1,8 +1,11 @@
 package be.iqit.user
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
 /**
  * Created by dvanroeyen on 30/11/15.
  */
+@JsonIgnoreProperties(ignoreUnknown = true)
 class User {
 
     String id
